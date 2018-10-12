5eb836e142c700afb89f49a30ffae27df50e1278
sudame <su.glasses.923@gmail.com>
2018-10-13 02:35:14 +0900

    delete: レイアウトを簡潔化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,5 @@
 <template>
   <div>
-    <!-- <global-header></global-header> -->
     <nuxt/>
   </div>
 </template>
-
-<script>
-import GlobalHeader from '~/components/GlobalHeader';
-
-export default {
-  components: {
-    GlobalHeader,
-  },
-};
-</script>
-
-
-<style lang="scss">
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
-</style>
