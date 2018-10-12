06fd461a97bcdd06b1c3257e6d5e0186f2a539ff
sudame <su.glasses.923@gmail.com>
2018-10-13 02:31:58 +0900

    add: コメントの追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="section">
+    <!-- ボタンをクリックするとgetProjectsListが発火 -->
     <button @click="getProjectsList">get</button>
     <div class="projects-list" v-for="project in projects" :key="project.id">
       {{ project }}
@@ -12,9 +13,11 @@ import { mapActions, mapState } from 'vuex';
 
 export default {
   computed: {
+    // vuexからprojectsを読み込み
     ...mapState(['projects']),
   },
   methods: {
+    // vuexからgetProjectsListを読み込み
     ...mapActions(['getProjectsList']),
   },
 };
--- store/index.js
+++ store/index.js
@@ -1,28 +1,42 @@
+// このファイルがvuex本体
+
+// vuexの読み込み
 import Vuex from 'vuex';
+// apollo-clientをラップして使いやすくしたものらしい
 import ApolloClient from 'apollo-boost';
+// gql(=GraphQL Query)を書きやすくするやつ
 import gql from 'graphql-tag';
+// fetchがグローバルに無い場合こいつを使う
+// はじめ開発してみたときここでコケたが、どうやらnuxtのサーバーサイドレンダリングが行われる際にfetchメソッドが無くてコケるらしい
 import fetch from 'node-fetch';
 
+// apollo clientのセットアップ
 const client = new ApolloClient({
+  // 独自のfetchメソッドを使う場合はここで設定する
   fetch: fetch,
+  // 取得先のベースURLを設定
   uri: 'https://online.sohosai.com/graphql',
 });
 
 export default () =>
   new Vuex.Store({
     state: {
+      // 企画情報の保管変数
       projects: [],
     },
     mutations: {
+      // 企画情報の更新ミューテーション
       updateProjects(state, newProjectsList) {
         state.projects = newProjectsList;
       },
     },
     actions: {
+      // 実際にクエリを叩いてもってくる部分
       getProjectsList({ commit }) {
-        console.log('hoge');
         client
           .query({
+            // graphql-tagの機能でクエリをカッコ良く書けている
+            // 全情報を取ってくるとレスポンスが悪いのでとりあえずidと企画名と企画団体名のみ取得
             query: gql`
               query {
                 result {
@@ -50,11 +64,15 @@ export default () =>
               }
             `,
           })
+          // クエリ結果はPromiseで返される。成功したらthen、失敗したらcatchの関数が実行される
           .then(data => {
+            // データ取得に成功したらupdateProjectsミューテーションに得られたデータをコミット
+            // 取得されるデータのデータ構造がよく分からないので汚い書き方になっている。公式ドキュメントが見当たらない。
             commit('updateProjects', data.data.result.projects);
           })
           .catch(error => {
-            console.log(error);
+            // データ取得に失敗したらエラーメッセージをブラウザコンソールに吐く。
+            console.error(error);
           });
       },
     },
